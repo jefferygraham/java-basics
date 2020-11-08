de97b57e07fe76eead1ec9abbdf1ca44ae7845e9
jeffery graham <jeffery.lemar.graham@gmail.com>
2020-11-08 16:59:09 -0500

    The final Keyword

---
+++ Lab-Final/src/com/jeffery/ClassA.java
@@ -0,0 +1,10 @@
+package com.jeffery;
+
+public class ClassA{
+
+  public String greeting = "Hello";
+
+  public final void someMethod(){
+    System.out.println("Some method.");
+  }
+}
---
+++ Lab-Final/src/com/jeffery/ClassB.java
@@ -0,0 +1,8 @@
+package com.jeffery;
+
+public class ClassB extends ClassA {
+
+	public final void someMethod(int X){
+		System.out.println("Some method.");
+	}
+}
